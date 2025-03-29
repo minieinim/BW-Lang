e78a219127fef7e28fa3a77d3902e2ce81528e1e
ieetnuts <ieetnuts@outlook.com>
2025-03-29 17:45:02 +0700

    Hello World

---
+++ main.py
@@ -0,0 +1,99 @@
+from enum import Enum
+
+ws = " \r\n\t"
+
+class types(Enum):
+ eror=-1
+ func=0
+ stri=1
+ numb=2
+ tbev=3
+
+def split(line:str) -> list[tuple[types,str]]:
+ res: list[tuple[types,str]] = []
+ sym:str = ""
+ i:int = 0
+ paren:int = 0
+ while i<len(line):
+  if line[i]=='"':
+   i+=1
+   while line[i]!='"' and i<len(line):
+    if line[i]=="\\":
+     i+=1
+     if line[i]=="n":
+      sym+="\n"
+     elif line[i]=="r":
+      sym+="\r"
+     elif line[i]=='"':
+      sym+='"'
+     elif line[i]=="\\":
+      sym+="\\"
+    else:
+     sym+=line[i]
+    i+=1
+   res.append((types.stri,sym))
+  elif line[i].isdigit():
+   while i<len(line) and line[i].isdigit():
+    sym+=line[i]
+    i+=1
+   res.append((types.numb,sym))
+  elif line[i]=="#": break
+  elif line[i]=="(":
+   paren+=1
+   i+=1
+   while True:
+    if line[i]=="(": paren+=1
+    elif line[i]==")": paren-=1
+    if paren==0 or i>=len(line): break
+    sym+=line[i]
+    i+=1
+   res.append((types.tbev,sym))
+  elif not line[i] in ws:
+   while i<len(line) and not line[i] in ws:
+    sym+=line[i]
+    i+=1
+   res.append((types.func,sym))
+  sym=""
+  i+=1
+ return res
+
+var:dict[tuple[str,tuple[types,str]]]={}
+def execute(args:list[tuple[int,str]]) -> tuple[int,str]:
+ global var
+ command:tuple[int,str] = args.pop(0)
+ if command[0] == types.stri or command[0] == types.numb:
+  return command
+ elif command[0] == types.tbev:
+  return execute(split(command[1]))
+ elif command[0] == types.func:
+  if command[1]=="print":
+   for i in args:
+    if i[0] == types.func:
+     print(execute(split(var[i[1]][1]))[1],end="")
+    else:
+     print(i[1],end="")
+  elif command[1]=="let":
+   if args[0][0] == types.func:
+    var[args[0][1]]=execute([args[1]])
+   else:
+    return (types.eror,"Cannot assign")
+  elif command[1]=="+":
+   try:
+    return (types.numb,str(int(execute(split(args[0][1]))[1]) + int(execute(split(args[1][1]))[1])))
+   except:
+    return (types.eror,"Cannot execute: '%s'" % command[1])
+  else:
+   return var[command[1]]
+ return (types.numb,"0")
+
+def main() -> int:
+ fin: FileIO = open("main.bw","r")
+ for line in fin.readlines():
+  t=split(line)
+  print(t)
+  execute(t)
+ fin.close()
+ return 0
+
+if __name__=="__main__":
+ main()
